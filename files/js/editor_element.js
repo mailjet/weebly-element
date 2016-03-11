2323e7620b3a36797ec61fa4e06011c189276b2a
FerhanGit <fismailov@mailjet.com>
2016-03-11 17:37:24 +0200

    slight text modifications

--- files/js/editor_element.js
+++ files/js/editor_element.js
@@ -3,11 +3,9 @@
  * @type {PlatformElement}
  */
 (function () {
+    
     var MailjetWidget = PlatformElement.extend({
         initialize: function () {
-            if('Paste your widget iFrame code here' != this.settings.get('widgetcode')) {
-                $('#instructions').remove();
-            }
             $(this.settings.get('widgetcode')).appendTo('.widgetcode');
         },
     });
